038f2a819f4fd22f12cee5acbb52b09c86ddcec7
Matheus Lucena <47263211+matheusepitacio@users.noreply.github.com>
2021-08-13 21:39:54 -0300

    Alterando para fazer a conexão com senha

--- ta-server/src/db/connection.ts
+++ ta-server/src/db/connection.ts
@@ -1,7 +1,7 @@
 const {Pool} = require('pg')
 
-const {user, database} = require('../../secrets.json')
+const {user, database, password} = require('../../secrets.json')
 
-const pool = new Pool({ user,database})
+const pool = new Pool({ user,database, password})
 
 export default pool;
